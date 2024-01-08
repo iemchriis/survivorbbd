df9e92b32117836224d54224bcc2eea98fa33550
iemchriis <71994685+iemchriis@users.noreply.github.com>
2024-01-08 18:01:23 +0800

    fixes

--- Assets/Scripts/Helpers/EnemyScript.cs
+++ Assets/Scripts/Helpers/EnemyScript.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class EnemyScript : MonoBehaviour
 {
     private Transform target;
+    private NavMeshAgent navmesh;
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindObjectOfType<PlayerMovement>().transform;
+        navmesh = GetComponent <NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         if (target != null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, 1 * Time.deltaTime);
+            navmesh.destination = target.position;
+            transform.LookAt(target.position);
+          //  transform.position = Vector3.MoveTowards(transform.position, target.position, 1 * Time.deltaTime);
         }
     }
 }
