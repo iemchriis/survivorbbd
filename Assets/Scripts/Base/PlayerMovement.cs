268cab5c1ab04294ac9319074e75f3e01d209e29
basilbenitez02 <basilbenitez@vimilu.com>
2024-01-09 16:02:48 +0800

    Enemy Targeting
    Targeting for Mobs

--- Assets/Scripts/Base/PlayerMovement.cs
+++ Assets/Scripts/Base/PlayerMovement.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private PlayerVision vision;
+
     [SerializeField] private Rigidbody rb;
     [SerializeField] private FixedJoystick joystick;
 
     public float moveSpeed;
 
+    private void Update()
+    {
+        LookAtEnemy();
+    }
+
     private void FixedUpdate()
     {
         MovePlayer();
+    }
+
+    void LookAtEnemy()
+    {
+        if (vision.nearestEnemy.transform.position == null)
+        {
+            transform.LookAt(Vector3.forward);
+        }
+        else
+        {
+            Vector3 enemyDirection = vision.nearestEnemy.transform.position;
+
+            transform.LookAt(enemyDirection);
+        }
+            
+
+        
     }
 
     void MovePlayer()
---
+++ Assets/Scripts/Base/PlayerVision.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerVision : MonoBehaviour
+{
+    public List<EnemyScript> targets;
+
+    
+    public GameObject nearestEnemy;
+
+
+   
+
+
+    public void CalculateNearest()
+    {
+        if (targets.Count <= 0)
+            return;
+
+        targets.Sort((x,y) => Vector3.Distance(transform.position, x.transform.position).CompareTo(Vector3.Distance(transform.position, y.transform.position)));
+
+
+        nearestEnemy = targets[0].gameObject;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+           EnemyScript enemy = other.GetComponent<EnemyScript>();
+
+            if(enemy != null)
+            {
+                if (targets.Contains(enemy))
+                    return;
+
+                targets.Add(enemy); 
+            }
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyScript enemy = other.GetComponent<EnemyScript>();
+
+            if (enemy != null)
+            {
+                targets.Remove(enemy);
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+        CalculateNearest();
+    }
+}
