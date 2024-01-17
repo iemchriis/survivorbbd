73cef2ee33fe4e16e172e7b2ce87682c12730e44
iemchriis <71994685+iemchriis@users.noreply.github.com>
2024-01-17 18:03:10 +0800

    HP canvas

--- Assets/Scripts/Base/PlayerHealth.cs
+++ Assets/Scripts/Base/PlayerHealth.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerHealth : CharacterBase
 {
-    
+    public Slider hpBar;
     public override void TakeDamage(int damage)
     {
         base.TakeDamage(damage);
         health -=damage;
+        hpBar.value -= damage;
         if(health <= 0)
         {
             Death();
