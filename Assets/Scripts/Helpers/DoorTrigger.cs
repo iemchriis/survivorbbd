a7b6f04720c7dcee7a1337df095377a2ce28191a
iemchriis <71994685+iemchriis@users.noreply.github.com>
2024-02-21 07:20:20 +0800

    exit indicator

--- Assets/Scripts/Helpers/DoorTrigger.cs
+++ Assets/Scripts/Helpers/DoorTrigger.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour
 {
     public int powerupIndex;
+    public GameObject[] powerups;
+
+    private void OnEnable()
+    {
+        randomOffer();
+    }
+
+
+    void randomOffer()
+    {
+        powerupIndex = Random.Range(0, powerups.Length);
+        powerups[powerupIndex].SetActive(true);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
